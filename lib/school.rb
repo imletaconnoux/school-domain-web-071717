28f31e819be6ea361ae5b33245be5aa57690705e
imletaconnoux <imletaconnoux@gmail.com>
2017-07-19 17:08:05 -0400

    passed tests

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+
+  attr_reader :roster
+
+   def initialize(name)
+     @name = name
+     @roster = {}
+   end
+
+   def add_student(name, grade)
+     if !@roster.has_key?(grade)
+       @roster[grade] = []
+     end
+     @roster[grade] << name
+   end
+
+   def grade(grade)
+     students_array = @roster[grade]
+     students_array
+   end
+
+   def sort
+     sorted = {}
+     @roster.each do |grade, students|
+       sorted[grade] = students.sort
+     end
+     sorted
+   end
+
+end
